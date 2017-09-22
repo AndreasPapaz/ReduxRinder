c7f4c5dcf2d6cdec2ebe716aeeaab3377e224ea9
Andreas <AndreasPapaz@gmail.com>
2017-09-22 18:14:48 -0500

    redux & react, reducers and null state handle

--- src/reducers/reducer_books.js
+++ src/reducers/reducer_books.js
@@ -1,8 +1,8 @@
 export default function() {
 	return [
-		{title: 'Harry Potter'},
-		{title: 'The Dark Tower'},
-		{title: 'Eloquent JavaScript'},
-		{title: 'Cracking the Coding Interview'},
+		{title: 'Harry Potter', pages: 101},
+		{title: 'The Dark Tower', pages: 23},
+		{title: 'Eloquent JavaScript', pages: 11},
+		{title: 'Cracking the Coding Interview', pages: 1}
 	]
 }
