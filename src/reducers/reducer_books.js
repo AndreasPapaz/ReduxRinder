cb6c07462f2587276969ea3a0b922b8b5fd3463b
Andreas <AndreasPapaz@gmail.com>
2017-09-22 14:42:41 -0500

    react & redux

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function() {
+	return [
+		{title: 'Harry Potter'},
+		{title: 'The Dark Tower'},
+		{title: 'Eloquent JavaScript'},
+		{title: 'Cracking the Coding Interview'},
+	]
+}
