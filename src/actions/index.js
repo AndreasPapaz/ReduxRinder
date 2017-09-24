92a4f9e3b0ada48d0b8ea4ac69b93866a2877ca8
Andreas <AndreasPapaz@gmail.com>
2017-09-24 12:22:34 -0500

    update

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,3 +1,8 @@
 export function selectBook(book) {
-	console.log('book has been selected', book.title);
+	//selectBook is an ActionCreator, it needs to return an action/
+	// an object w/ a type prop
+	return {
+		type: 'BOOK_SELECTED',
+		payload: book
+	}
 }
